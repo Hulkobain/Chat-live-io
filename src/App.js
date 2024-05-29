a08ed27717b6bfd6dfecfc083294daf60e19fb4b
Hulkobain <114093499+Hulkobain@users.noreply.github.com>
2024-05-29 19:51:59 +0200

    Create containers AddMessage/Message

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="container">
+      <div id="container">
         <Sidebar />
         <section id="main">
           <MessagesList />
